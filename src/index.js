871be40badab0e0bf26f78833ea074a1da96833b
Luiz Batanero <luizbatanero@gmail.com>
2019-07-17 17:22:28 -0300

    Setup auth routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Routes from './routes';
+
 export default function App() {
-  return (
-    <View>
-      <Text>GoBarber</Text>
-    </View>
-  );
+  return <Routes />;
 }
