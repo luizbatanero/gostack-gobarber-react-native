808d9e0d926252a373785dad93c642798951cfee
Luiz Batanero <luizbatanero@gmail.com>
2019-07-17 18:12:44 -0300

    Config Reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,17 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+const host = '192.168.15.10';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ host })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
