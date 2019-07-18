4bb383dd038b4605365bb469d4769258f940917f
Luiz Batanero <luizbatanero@gmail.com>
2019-07-17 21:58:29 -0300

    Select provider page

---
+++ src/pages/New/SelectDateTime/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
+// import { Container } from './styles';
+
+export default function SelectDateTime() {
+  return <Background />;
+}
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('SelectProvider');
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
